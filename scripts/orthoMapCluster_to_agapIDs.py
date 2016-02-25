a80655dd2f8d431143bad71873a839703f6713d1
a1ultima <a1ultima@gmail.com>
2016-02-25 20:00:04 +0000

    completed the OrthoClusterCoordinate -to- AGAP ID mapper, generates a dict that is pickled in ../data/*.p and a flat file

--- scripts/orthoMapCluster_to_agapIDs.py
+++ scripts/orthoMapCluster_to_agapIDs.py
@@ -2,41 +2,145 @@
 # Imports
 #
 from os.path import relpath as rp 
+import pickle
+import os.path
+import csv
 
 #
-# OG-to-AGAP file
+# File system
 #
-try:
-	path_og_to_agap = rp("/home/nho/maps/AnOrthoMap/output/OGs_AGAP.txt")
-	fi = open(path_og_to_agap,"r")
-except IOError:
-	path_og_to_agap = "../data/OGs_AGAP.txt"
-	fi = open(path_og_to_agap,"r")
+if not os.path.isdir("../data/"):
+	os.makedirs("../data/")
 
-#
-# OG-to-AGAP dict
-#
-og_to_agap = {}
+pickle_name = 'oc_to_agap'
 
-while True: 
+if (os.path.isfile("../data/"+pickle_name+'.p') and os.path.isfile("../data/"+pickle_name+'.tsv')):
+    print('WARNING: unpickling old data...')
+    oc_to_agap = pickle.load(open("../data/"+pickle_name+'.p','rb'))
 
-	line = fi.readline()
-
-	if line=="":
-		break
-
-	line_split = line.split("\t")
-
-	og 	= line_split[0]
-	agap= line_split[1].rstrip()
-
+else:
+	print ("Mapping OrthoCluster Coordinates to AGAP IDs...")
+	#
+	# OG-to-AGAP file
+	#
 	try:
-		og_to_agap[og].append(agap)
-	except KeyError:
-		og_to_agap[og] = [agap]
+		path_og_to_agap = rp("/home/nho/maps/AnOrthoMap/output/OGs_AGAP.txt")
+		fi = open(path_og_to_agap,"r")
+	except IOError:
+		path_og_to_agap = "../data/OGs_AGAP.txt"
+		fi = open(path_og_to_agap,"r")
 
-fi.close()
+	#
+	# OG-to-AGAP dict
+	#
+	og_to_agap = {}
 
-#
-#
-#
+	while True: 
+
+		line = fi.readline()
+
+		if line=="":
+			break
+
+		line_split = line.split("\t")
+
+		og 	= line_split[0]
+		agap= line_split[1].rstrip()
+
+		try:
+			og_to_agap[og].append(agap)
+		except KeyError:
+			og_to_agap[og] = [agap]
+
+	fi.close()
+
+
+	#
+	# OrthoCluster-to-OG file
+	#
+	try:
+		path_og_to_agap = rp("/home/nho/maps/AnOrthoMap/output/OG-coords.csv")
+		fi = open(path_og_to_agap,"r")
+	except IOError:
+		path_og_to_agap = "../data/OG-coords.csv"
+		fi = open(path_og_to_agap,"r")
+
+	#
+	# OrthoClusterCoords-to-OG dict
+	#
+	oc_to_og = {}
+
+	fi.readline() # headers
+
+	while True: 
+
+		line = fi.readline()
+
+		if line=="":
+			break
+
+		line_split = line.split(",")
+
+		og 	= line_split[0]
+		x 	= int(line_split[1])
+		y 	= int(line_split[2].rstrip())
+
+		try:
+			oc_to_og[x,y].append(og)
+		except KeyError:
+			oc_to_og[x,y] = [og]
+
+	fi.close()
+
+	#
+	# Join: OrthoClusterCoords-to-AGAP
+	#
+	oc_to_agap = {}
+
+	for oc in oc_to_og.keys():
+
+		ogs = oc_to_og[oc]
+
+		for og in ogs:
+
+			agaps = og_to_agap[og]	
+
+			try:
+				oc_to_agap[oc] += agaps
+			except KeyError:
+				oc_to_agap[oc] = agaps
+
+	#
+	# write dict to file 
+	#
+	fo = open("../data/oc_to_agap.tsv","w")
+
+	print "writing to file..."
+
+	fo.write("X\tY\tAgapIDs"+"\n")
+
+	for oc in oc_to_agap.keys():
+		x = oc[0]
+		y = oc[1]
+		fo.write(str(x)+"\t"+str(y)+"\t"+"\t".join(oc_to_agap[oc])+"\n")
+
+	fo.close()
+
+	#
+	# dump the dict into pickle file
+	# 
+	pickle.dump(oc_to_agap,open("../data/"+pickle_name+'.p','wb'))
+
+	print "Complete!"
+
+
+
+
+
+
+
+
+
+
+
+
